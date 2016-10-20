7add56f3ef89224034cd388fe31852b13da775d4
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-10-20 14:28:17 +0200

    Allow CORS

--- server.js
+++ server.js
@@ -3,16 +3,27 @@ var bodyParser = require('body-parser');
 var SpellChecker = require('./src/NodehunSpellchecker')
 
 var PORT = process.env.PORT || 4777
+var config = require('./config')
+var spellchecker = new SpellChecker(config)
+
 var app = express()
+// for json bodies
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-var config = require('./config')
-var spellchecker = new SpellChecker(config)
+// for CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
+// API
 
 // get corrections for a piece of plain-text
 app.post('/api/check', function (req, res) {
   var text = req.body.text;
+  console.log('### received check request for', text, req.body)
   spellchecker.check(text, function(err, result) {
     if (err) {
       res.status(500).send('Internal Error')
@@ -23,5 +34,5 @@ app.post('/api/check', function (req, res) {
 })
 
 app.listen(PORT, function () {
-  console.info('Example app listening on port %s!', PORT);
+  console.info('Spellchecker Service running port %s.', PORT);
 })
